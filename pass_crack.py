6d8f71872b2985696db35455327e04759acf41ce
Vaibhav Rajput <73458444+vaibhavr24@users.noreply.github.com>
2021-11-14 00:12:38 +0530

    Update pass_crack.py

--- pass_crack.py
+++ pass_crack.py
@@ -6,13 +6,14 @@ guess = ''
 
 lowalphalist = list(string.ascii_lowercase)
 uppralphalist = list(string.ascii_uppercase)
-passw = lowalphalist + uppralphalist
+numbercount = [1,2,3,4,5,6,7,8,9,0]
+passw = lowalphalist + uppralphalist + numbercount
 
 
 while(guess!=userpass):
     guess = ''
     for ele in range(len(userpass)):
-        guess_let = passw[randint(0,51)]
+        guess_let = passw[randint(0,60)]
         guess = str(guess_let) + str(guess)
 
 print('your password is: ',guess)
